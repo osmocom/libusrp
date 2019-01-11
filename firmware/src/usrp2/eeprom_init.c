0934c3785035459a869b0a34e65382ecaee89018
Harald Welte <laforge@gnumonks.org>
2019-01-11 17:44:26 +0100

    firmware: Port to more modern SDCC syntax.
    SDCC, at least from version 3 onwards, has change the names of special
    attributes like "at" or "sfr".  They are now all prefixed with double-
    underscores.
    
    Upstream fx2lib has made that change in commit 3071389d9a868ed553b84809b399008648536fcf
    which can be obtained from
    https://github.com/djmuhlestein/fx2lib/pull/1/commits/3071389d9a868ed553b84809b399008648536fcf
    
    Change-Id: I5df64bc3cc09402bdfacec9c256e96135e3688fd

--- firmware/src/usrp2/eeprom_init.c
+++ firmware/src/usrp2/eeprom_init.c
@@ -29,7 +29,7 @@
  * into hash1.
  */
 #define	  USRP_HASH_SIZE      16
-xdata at USRP_HASH_SLOT_0_ADDR unsigned char hash0[USRP_HASH_SIZE];
+__xdata __at USRP_HASH_SLOT_0_ADDR unsigned char hash0[USRP_HASH_SIZE];
 
 
 #define enable_codecs() USRP_PA &= ~(bmPA_SEN_CODEC_A | bmPA_SEN_CODEC_B)
