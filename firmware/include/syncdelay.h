4561fc7ee131321da51736f25d9396949c723a74
Harald Welte <laforge@gnumonks.org>
2019-01-11 17:44:26 +0100

    firmware: Port to more modern SDCC syntax.
    SDCC, at least from version 3 onwards, has change the names of special
    attributes like "at" or "sfr".  They are now all prefixed with double-
    underscores.
    
    Upstream fx2lib has made that change in commit 3071389d9a868ed553b84809b399008648536fcf
    which can be obtained from
    https://github.com/djmuhlestein/fx2lib/pull/1/commits/3071389d9a868ed553b84809b399008648536fcf
    
    Change-Id: I5df64bc3cc09402bdfacec9c256e96135e3688fd

--- firmware/include/syncdelay.h
+++ firmware/include/syncdelay.h
@@ -58,8 +58,8 @@
 /*
  * FIXME ensure that the peep hole optimizer isn't screwing us
  */
-#define	SYNCDELAY	_asm nop; nop; nop; _endasm
-#define	NOP		_asm nop; _endasm
+#define	SYNCDELAY	__asm nop; nop; nop; __endasm
+#define	NOP		__asm nop; __endasm
 
 
 #endif /* _SYNCDELAY_H_ */
